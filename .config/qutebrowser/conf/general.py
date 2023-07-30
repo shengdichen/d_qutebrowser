238a6e040834f1dbfa87f8d7c418e1abc18b76e8
shc <sh@c>
2023-07-30 23:14:47 +0200

    Do NOT auto-save session

--- .config/qutebrowser/conf/general.py
+++ .config/qutebrowser/conf/general.py
@@ -14,8 +14,8 @@ class MiscConf:
         self._config.set("changelog_after_upgrade", "never")
 
     def _default_pages(self) -> None:
+        self._config.set("auto_save.session", False)
         self._config.set("session.default_name", "def")
-        self._config.set("auto_save.session", True)
 
         self._config.set("url.start_pages", "https://shengdichen.xyz")  # on launch
         self._config.set("url.default_page", "about:blank")  # when opening new tab
