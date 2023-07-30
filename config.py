8b8c7f3f40024fa81da668ad265d4e84f66877f8
shc <sh@c>
2023-07-30 16:55:38 +0200

    Move general-conf into own module

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from conf import visual, bind, webcontent
+from conf import visual, bind, webcontent, general
 
 # REF:
 #   qute://help/configuring.html
@@ -9,45 +9,10 @@ class Configuration:
     def __init__(self, config):
         self._config = config
 
-        self._misc()
+        general.MiscConf(self._config)
         webcontent.WebContent(self._config)
-        self._edit()
         self._visual()
         self._bind()
-
-    def _misc(self) -> None:
-        # only use self._config.py
-        self._config.load_autoconfig(False)
-
-        self._config.set("changelog_after_upgrade", "never")
-
-        self._config.set("auto_save.session", True)
-
-    def _edit(self) -> None:
-        self._config.set(
-            "editor.command",
-            [
-                "foot",
-                "nvim",
-                "{file}",
-                "-c",
-                "normal {line}G{column0}l",
-            ],
-        )
-
-        self._config.set(
-            "spellcheck.languages",
-            [
-                "en-US",
-                "fr-FR",
-                "sv-SE",
-                "de-DE",
-                "it-IT",
-                "es-ES",
-                "pt-BR",
-                "ru-RU",
-            ],
-        )
 
     def _visual(self) -> None:
         v = visual.Visual(self._config)
