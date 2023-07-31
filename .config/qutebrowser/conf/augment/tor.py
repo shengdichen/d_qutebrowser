91437b94054ac43d64992865c2528aa425bf226a
shc <sh@c>
2023-07-31 13:31:03 +0200

    Separate _make_engines() from c'tor

--- .config/qutebrowser/conf/augment/tor.py
+++ .config/qutebrowser/conf/augment/tor.py
@@ -3,8 +3,19 @@ from ..util.cmd import Cmd
 
 class Tor:
     def __init__(self):
-        self._config_engines = "url.searchengines"
+        (
+            self._config_engines,
+            self._engines_activated,
+            self._engines_deactivated,
+        ) = self._make_engines()
 
+        self._config_proxy = "content.proxy"
+        self._proxy_activated, self._proxy_deactivated = (
+            "socks://localhost:9050/",
+            "system",
+        )
+
+    def _make_engines(self) -> tuple[str, dict, dict]:
         engines_nontor = {
             "duck": self._make_duck_search_string("https://duckduckgo.com/"),
             "brave": self._make_brave_search_string("https://search.brave.com/"),
@@ -17,23 +28,21 @@ class Tor:
                 "https://search.brave4u7jddbv7cyviptqjc7jusxh72uik7zt6adtckl5f4nwy2v72qd.onion/"
             ),
         }
-        self._engines_activated = {
-            "DEFAULT": engines_tor["duck"],
-            "d": engines_tor["duck"],
-            "dn": engines_nontor["duck"],
-            "b": engines_tor["brave"],
-            "bn": engines_nontor["brave"],
-        }
-        self._engines_deactivated = {
-            "DEFAULT": engines_nontor["duck"],
-            "d": engines_nontor["duck"],
-            "b": engines_nontor["brave"],
-        }
 
-        self._config_proxy = "content.proxy"
-        self._proxy_activated, self._proxy_deactivated = (
-            "socks://localhost:9050/",
-            "system",
+        return (
+            "url.searchengines",
+            {
+                "DEFAULT": engines_tor["duck"],
+                "d": engines_tor["duck"],
+                "dn": engines_nontor["duck"],
+                "b": engines_tor["brave"],
+                "bn": engines_nontor["brave"],
+            },
+            {
+                "DEFAULT": engines_nontor["duck"],
+                "d": engines_nontor["duck"],
+                "b": engines_nontor["brave"],
+            },
         )
 
     @staticmethod
