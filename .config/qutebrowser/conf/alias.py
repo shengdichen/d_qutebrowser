d4f6254acabff7944818b2db47da06fafcc23b42
shc <sh@c>
2023-07-31 11:22:45 +0200

    Add f'n for switch'g tor on/off

--- .config/qutebrowser/conf/alias.py
+++ .config/qutebrowser/conf/alias.py
@@ -2,14 +2,27 @@ class Alias:
     def __init__(self, config):
         self._config = config
 
+        self._aliases = {}
+
     def apply(self) -> None:
-        self._config.set(
-            "aliases",
-            {
-                "q": "close",
-                "qa": "quit",
-                "w": "session-save",
-                "wq": "quit --save",
-                "wqa": "quit --save",
-            },
-        )
+        self._exit()
+        self._tor_activate()
+        self._tor_deactivate()
+
+        self._config.set("aliases", self._aliases)
+
+    def _exit(self) -> None:
+        self._aliases |= {
+            "w": "session-save",
+            "wq": "quit --save",
+            "q": "close",
+            "q!": "quit",
+        }
+
+    def _tor_activate(self) -> None:
+        cmd = " ".join(["set", "content.proxy", "socks://localhost:9050/"])
+        self._aliases.update(tor_on=cmd)
+
+    def _tor_deactivate(self) -> None:
+        cmd = " ".join(["set", "content.proxy", "system"])
+        self._aliases.update(tor_off=cmd)
