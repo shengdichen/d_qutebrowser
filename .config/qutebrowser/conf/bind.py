df956aa1db26c413049ede11613c50ffc271f5b4
shc <sh@c>
2023-08-02 12:52:17 +0200

    Decrease scroll'g step of |d|&|u|

--- .config/qutebrowser/conf/bind.py
+++ .config/qutebrowser/conf/bind.py
@@ -130,8 +130,8 @@ class ModeNormal(_ModeSpecific):
         self._bind("L", Cmd.concat(Cmd.repeat(cmd_right, 4)))
 
         cmd_scroll = "scroll-page "
-        self._bind("u", f"{cmd_scroll}0 -0.5")
-        self._bind("d", f"{cmd_scroll}0 +0.5")
+        self._bind("u", f"{cmd_scroll}0 -0.37")
+        self._bind("d", f"{cmd_scroll}0 +0.37")
         self._bind("b", f"{cmd_scroll}0 -1.0")
         self._bind("f", f"{cmd_scroll}0 +1.0")
 
